0f4b9a9ffb9d19434480eaede9d79519189b415b
Will Hellinger <111201130+Will-Hellinger@users.noreply.github.com>
2023-10-17 15:14:58 -0400

    Compress nbs_format_song.py

--- nbs_format_song.py
+++ nbs_format_song.py
@@ -3,27 +3,17 @@ import os
 from constants import *
 
 def get_valid_input(valid_inputs: list, msg: str) -> str:
-  valid = False
-
-  while not valid:
+  while True:
     user_input = input(msg)
 
     if user_input in valid_inputs:
-      valid = True
-    else:
-      print(f'"{user_input}" is not a valid input.')
-  
-  return user_input
+      return user_input
+    
+    print(f'"{user_input}" is not a valid input.')
 
 
 def remove_custom_notes(chord: list) -> list:
-  new_chord = []
-
-  for note in chord:
-    if note.instrument <= 15:
-      new_chord.append(note)
-    
-  return new_chord
+  return [note for note in chord if note.instrument <= 15]
 
 
 def fix_illegal_notes(chord: list) -> list:
@@ -50,13 +40,11 @@ def remove_helper(chord: list, chord_max_size: int, type: str) -> list:
   first_note = chord[0]
 
   for note in chord:
-    if type == 'high':
-      if note.key > first_note.key:
-        first_note = note
+    if type == 'high' and (note.key >> first_note.key):
+      first_note = note
 
-    elif type == 'low':
-      if note.key < first_note.key:
-        first_note = note
+    elif type == 'low' and (note.key << first_note.key):
+      first_note = note
   
   chord.remove(first_note)
 
@@ -108,78 +96,63 @@ def remove_chord_violations(chord: list) -> list:
 
 
 def main() -> None:
-  compress_song = False
-  max_chord_violation = False
-
-  while True:
     song_file = input('Please enter the file name of your song (include the .nbs): ')
 
-    if os.path.exists(song_file) == False:
-      print('File does not exist. Please try again.')
+    while not os.path.exists(song_file):
+        print('File does not exist. Please try again.')
+        song_file = input('Please enter the file name of your song (include the .nbs): ')
 
-    else:
-      try:
+    try:
         song = pynbs.read(song_file)
-        song_name = song_file[:song_file.find('.nbs')]
-        break
+        song_name = song_file.replace('.nbs', '')
+    except Exception as error:
+        print(f'Error: {error}')
+        return
 
-      except Exception as error:
-        print(f'error: {error}')
+    if song.header.song_length > MAX_SONG_LENGTH:
+        print(f'Your song\'s length is {song.header.song_length}, and the max length of a song is {MAX_SONG_LENGTH}.')
+        print('You might want to compress your song if it is too slow or too long.')
+        print('Compressing your song would remove every other tick and make it half as long. This may or may not make your song sound much worse.')
+        compress_choice = get_valid_input(['y', 'n'], 'Would you like to compress your song? (y/n): ')
+        compress_song = (compress_choice == 'y')
 
-  # give user option to compress song
-  if song.header.song_length > MAX_SONG_LENGTH:
-    print(f'Your song\'s length is {song.header.song_length}, and the max length of a song is {MAX_SONG_LENGTH}.')
-  
-  print('You might want to compress your song if it is too slow or too long.')
-  print('Compressing your song would remove every other tick and make it half as long. This may or may not make your song sound much worse.')
+    for note in song.notes:
+        if note.key < INSTRUMENT_RANGE[0] or note.key > INSTRUMENT_RANGE[1]:
+            print('Your song contains notes that are outside the normal range. They will be transposed to be playable.')
+            input('Press Enter to Continue')
+            break
 
-  if get_valid_input(['y', 'n'], 'Would you like to compress your song? (y/n): ') == 'y':
-    compress_song = True
+    if len(song.instruments) > 0:
+        print('Your song contains custom instruments. All notes using custom instruments will be removed.')
+        input('Press Enter to Continue')
 
-  # warn user if there are notes out of range
-  for note in song.notes:
-    if note.key < INSTRUMENT_RANGE[0] or note.key > INSTRUMENT_RANGE[1]:
-      print('Your song contains notes that are outside the normal range. They will be transposed to be playable.')
-      input('Press Enter to Continue')
-      break
-  
-  # warn user if there are custom instruments
-  if len(song.instruments) > 0:
-    print('Your song contains custom instruments. All notes using custom instruments will be removed.')
-    input('Press Enter to Continue')
-  
-  newSong = pynbs.new_file()
-  newSong.header, newSong.layers, newSong.header.tempo = song.header, song.layers, 5
+    newSong = pynbs.new_file()
+    newSong.header, newSong.layers, newSong.header.tempo = song.header, song.layers, 5
 
-  # iterate through the whole song by chords
-  for tick, chord in song:
-    newTick = tick
+    max_chord_violation = False
 
-    if compress_song == True:
-      newTick = tick // 2
-    
-    if newTick > MAX_SONG_LENGTH:
-      print('Notice: Your song was too long, so some had to be cut off the end.')
-      break
-  
-    if (tick % 2 != 0 and compress_song == False) or (tick %2 == 0):
-      chord = remove_custom_notes(chord)
-      chord = fix_illegal_notes(chord)
-      [chord, max_chord_violation] = remove_chord_violations(chord)
-      
-      for note in chord:
-        note.tick, note.panning, note.pitch = newTick, 0, 0
-        
-        newSong.notes.append(note)
-  
-  if max_chord_violation == True:
-    print('Notice: Your song contained chords that were larger than allowed. Some notes were removed from these chords.')
+    for tick, chord in song:
+        newTick = tick if not compress_song else tick // 2
 
-  # save the new song
-  new_file_name = f'{song_name}-(Formatted).nbs'
+        if newTick > MAX_SONG_LENGTH:
+            print('Notice: Your song was too long, so some had to be cut off the end.')
+            break
 
-  newSong.save(new_file_name)
-  print('Your formatted song was saved under "', new_file_name, '"')
+        if (tick % 2 != 0 and not compress_song) or (tick % 2 == 0):
+            chord = remove_custom_notes(chord)
+            chord = fix_illegal_notes(chord)
+            chord, max_chord_violation = remove_chord_violations(chord)
+
+            for note in chord:
+                note.tick, note.panning, note.pitch = newTick, 0, 0
+                newSong.notes.append(note)
+
+    if max_chord_violation:
+        print('Notice: Your song contained chords that were larger than allowed. Some notes were removed from these chords.')
+
+    new_file_name = f'{song_name}-(Formatted).nbs'
+    newSong.save(new_file_name)
+    print(f'Your formatted song was saved under "{new_file_name}"')
 
 
 if __name__ == '__main__':
